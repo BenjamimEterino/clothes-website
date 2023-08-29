6037a41a8766e9f5de537961ce3ce94268bc6515
BenjamimEterino <benjamimtoretto901@gmail.com>
2023-08-29 15:50:52 +0200

    aa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,52 @@
 function changeDisplay(list) {
-    var menu = document.getElementById(list);
+  var menu = document.getElementById(list);
 
-    if (menu.style.display == "block") {
-       menu.style.display="none"
-    } else {
-        menu.style.display="block"
-    }
+  if (menu.style.display == "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
 }
 
 function changeButton(x) {
-    x.classList.toggle("change");
-  }
+  x.classList.toggle("change");
+}
+
+$('.owl-carousel').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    loop: true,
+    margin: 50,
+    responsiveClass: true,
+    nav: true,
+    loop: true,
+    stagePadding: 100,
+    responsive: {
+      0: {
+        items: 1
+      },
+      568: {
+        items: 2
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 3
+      }
+    }
+  })
+  $(document).ready(function() {
+    $('.popup-youtube').magnificPopup({
+      disableOn: 320,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: true
+    });
+  });
+  $('.item').magnificPopup({
+    delegate: 'a',
+  });
